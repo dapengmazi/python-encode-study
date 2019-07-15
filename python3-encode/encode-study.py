05b81f16ed84b549bb775ac5db7fb0547421a1d4
davidmaster <davidmaster@163.com>
2019-07-15 12:44:49 +0800

    ord

--- python3-encode/encode-study.py
+++ python3-encode/encode-study.py
@@ -1,5 +1,10 @@
 import sys
 
+print(format(ord('A'),'b')) #二进制表示
+print(ord('A')) # 十进制表示
+print(format(ord('a'),'b')) #二进制表示
+print(ord('a')) # 十进制表示
+
 #查看单个字符的二进制，十进制，十六进制表示
 print(format(ord('鹏'),'b')) #二进制表示
 print(ord('鹏')) # 十进制表示
